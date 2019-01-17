bdfd3bdf062a9a50034b566080accf97f4a68401
nestorgrandal <nestor.a.grandal@gmail.com>
2019-01-17 15:47:52 -0500

    Filtered rows with biomass <60, altered Total to TotalG, converted TotalG from mg to g, abbreviated the strings under the Nutrients column, and replaced all periods with commas in TotalG

--- DataMixer.R
+++ DataMixer.R
@@ -2,4 +2,9 @@ Data=read.csv("InData.csv")
 install.packages("dplyr")
 library(dplyr)
 x=select(Data,Total,Taxon,Scenario,Nutrients)
-head(x)
+TotalL60=filter(x,Total>60)
+TotalL60 %>%
+  rename(TotalG=Total) %>%
+  mutate(TotalG = TotalG/1000) %>%
+  mutate(Nutrients = abbreviate(Nutrients,minlength=1)) %>%
+  mutate(TotalG = gsub("\\.",",",TotalG))
